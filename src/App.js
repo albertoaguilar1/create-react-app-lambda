8e0940823d7661d41a15ded9db75ffbc13622c98
alberto aguilar <faraond1@gmail.com>
2020-08-20 11:09:38 -0500

    cambios test

--- src/App.js
+++ src/App.js
@@ -36,15 +36,25 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
+          <img src={logo} className="App-logo" alt="logo"></img>
         </header>
+        <body>
+          <div className="app container">
+            <div clasName="jumnbotron">
+              <p className="lead text-cemter">Buscador de productos</p>
+
+            </div>
+            <h1>Pagina en construccion</h1>
+            <h2>pagina en construccion</h2>
+          </div>
+        </body>
       </div>
     )
   }
 }
+
+
+
+
 
 export default App
